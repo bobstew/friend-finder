b038a9fa6286f186a4ce0b24a52ab7a12fe8ed2b
Robert <robstewjob@gmail.com>
2018-02-27 22:34:33 -0600

    works and cleaned

---
+++ app/data/friends.js
@@ -0,0 +1,3 @@
+// Includes a single default profile, so that anyone demoing from the GitHub repository will have a result.
+module.exports = [{"name":"Bob","photo":"https://graphicpolicy.files.wordpress.com/2016/03/the-tick.jpg","answers":["2","5","5","4",
+					"5","1","1","2","2","2"]}];
